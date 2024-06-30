eb03e63db2c1c62c3f50d319618e8658a9f26801
joicesezm <136086518+joicesezm@users.noreply.github.com>
2024-06-30 19:31:36 -0300

    Update cadastroCargos.php

--- Projeto Integrador/Admin/cadastroCargos.php
+++ Projeto Integrador/Admin/cadastroCargos.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de cargos</title>
+    <title>Cargos</title>
     <link rel="stylesheet" href="../cadastroCargo.css">
     <style>
         body {
