a6ab0e1deda08511a1e787bc527ac8edaf57ede8
DeividSouza93 <deividsouza93@hotmail.com>
2024-07-02 10:08:04 -0300

    02/07/2024

--- Projeto Integrador/Admin/cadastroVagas.php
+++ Projeto Integrador/Admin/cadastroVagas.php
@@ -1,7 +1,7 @@
 
 <?php
  session_start();
- include("conexao.php");
+ require_once("..\Data\conexao.php");  
 
 // Verifica se os dados foram submetidos via método POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") 
@@ -9,24 +9,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $nomeEmpresa = $_POST["nomeEmpresa"];
     $cargo = $_POST["cargo"];
     $email = $_POST["email"];
-    $localTrabalho = $_POST["localTrabalho"];
+    $localTrabalho = $_POST["local"];
     $requisitos = $_POST["requisitos"];
     $descricao = $_POST["descricao"];
-    $turno
-     = $_POST["turno"];
+    $turno = filter_input(INPUT_POST, "turno");
 
 
     // Prepara e executa a consulta SQL para inserir os dados na tabela
-    $sql = "INSERT INTO vaga (nomEmpresa, idCargo, email, endereco , requisitos, descAtividades, turno)
+    $sql = "INSERT INTO vaga (nomEmpresa, cargo, email, endereco , requisitos, descAtividades, turno)
             VALUES ('$nomeEmpresa', '$cargo', '$email', '$localTrabalho', '$requisitos', '$descricao', '$turno')";
         $resultadoVagas = mysqli_query ($conexao, $sql);
 
 if ($resultadoVagas) {
     echo "<script>alert('Dados cadastrados com sucesso!');</script>";
-    header("Location: /home.php");
+   // header("Location: /home.php");
 
 } else {
-    echo "<script>alert('ERRO!');</script>". $conexao->error;
+   // echo "<script>alert('ERRO!');</script>". $conexao->error;
 }
 
 // Fecha a conexão com o banco de dados
